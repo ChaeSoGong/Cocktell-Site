8fe7e5d80391add594d6c07c29229e6d3d2c2c42
ChaeDoll <dlacodbs0627@gmail.com>
2023-08-16 16:44:55 +0900

    fix

--- src/app/(main-page)/mybag/page.js
+++ src/app/(main-page)/mybag/page.js
@@ -1,8 +1,7 @@
-import { testFETCH } from "@/app/api/route"
-
+// import { testFETCH } from "@/app/api/route"
 export default async function MyBag() {
-  const data = {"users":[{email:"hi"}]};
-  testFETCH('POST', data);
+/*   const data = {"users":[{email:"hi"}]};
+  testFETCH('POST', data); */
   //const cocktailData = await testFETCH("GET");
   //const newData = cocktailData["users"]; //데이터베이스에서 "users" 선택
   //cocktailData["users"].filter((data)=>{data.key>3})
