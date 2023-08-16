01d2e06d74abfb9e4bb80f214d699007d9828a57
ChaeDoll <dlacodbs0627@gmail.com>
2023-08-16 18:52:00 +0900

    updtae

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default {
-  compiler: {
-    styledComponents: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    loader: 'ChaeDoll',
-    path: isProduction ? 'https://ChaeSoGong.github.io/' : 'http://localhost:3000',
-  },
-  assetPrefix: isProduction ? '/premium-page' : '',
-};
-
-/* const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        styledComponents: true
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      images: {
+        loader: 'ChaeDoll',
+        path: isProduction ? 'https://ChaeSoGong.github.io/' : 'http://localhost:3000',
+      },
+      assetPrefix: isProduction ? '/premium-page' : '',
+}
 
 module.exports = nextConfig
 
- */
