79c8ca03ae2d2c07dd95fc494abb578ec3635ea5
ChaeDoll <dlacodbs0627@gmail.com>
2023-08-23 14:04:52 +0900

    um

--- src/app/(client-component)/header.js
+++ src/app/(client-component)/header.js
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
 
 export default function Header(){ //Developed by 임채윤
