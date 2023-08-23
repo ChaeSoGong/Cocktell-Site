8fab495e8cd7f40b8d04c3c28e11e44afb81c37e
ChaeDoll <dlacodbs0627@gmail.com>
2023-08-23 13:53:04 +0900

    up

--- src/app/(admin-page)/user-manage/page.js
+++ src/app/(admin-page)/user-manage/page.js
@@ -1,10 +1,10 @@
 import { getUsersData } from '@/app/api/users-get';
 export default async function UserManage() {
-    const usersData = await getUsersData() //Users DB에서 users Object 리스트 가져오기
+    // const usersData = await getUsersData() //Users DB에서 users Object 리스트 가져오기
     // console.log(usersData);
     return (
       <div className='user-manage'>
-        {usersData.map((user)=>{
+{/*         {usersData.map((user)=>{
             return(<div key={user.id} className='m-4 bg-gray-300'>
                 {`상태 : ${user.state}`}<br/>
                 {`이메일 : ${user.email}`}<br/>
@@ -14,7 +14,7 @@ export default async function UserManage() {
                 {`성별 : ${user.gender}`}<br/>
                 {`가입날짜 : ${user.joindate}`}<br/>
             </div>)
-        })}
+        })} */}
       </div>
     );
 };
