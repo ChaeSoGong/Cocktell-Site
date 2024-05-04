5ce0051cfbe0bdbdd69925b3db79ecdddd6f489f
ChaeDoll <dlacodbs0627@gmail.com>
2024-05-04 16:44:12 +0900

    240504

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 import Link from 'next/link'
 import { BiSearchAlt2 } from "react-icons/bi";
 import { BiSolidCalculator } from "react-icons/bi";
-import { noto_sans, playfair_display, diphylleia } from './fonts'
+import { noto_sans, playfair_display } from './fonts'
 import Header from './(client-component)/header.js' //Header Component
 
 export const metadata = {
